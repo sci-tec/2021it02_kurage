9709892947d9af0b07eb9d6b60a3721866e4294a
takae <2020it10@sci-tec.ac.jp>
2022-02-04 10:37:09 +0900

    水質

--- app.js
+++ app.js
@@ -165,6 +165,13 @@ let date_time = ()=>{
     $('.kurage-size').text((Math.round((kurage.size+(elapsed("Tsize")*0.0013))*1000)/1000)+"mm");
     //今の満腹度、水質を表示
     $('.kurage-Satisfaction').text(100-Math.floor(elapsed("Teat")/3456)+"%");
+    let suishitu = (1-((100-Math.floor(elapsed("Twater")/6048))/100));
+    if(suishitu<0.8){
+        $('#lLight').css({"opacity" : 1-((100-Math.floor(elapsed("Twater")/6048))/100)});
+    } else {
+        $('#lLight').css({"opacity" : 0.8});
+    }
+    console.log(suishitu);
     $('.kurage-Water').text(100-Math.floor(elapsed("Twater")/6048)+"%");
     //満腹度か水質が0%になったら,リセットされる
     if(Math.floor(elapsed("Teat")/3456)>=100||Math.floor(elapsed("Twater")/6048)>=100){
